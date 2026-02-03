7e1b381e8843155c327bf2edb9babe11b9c49eb8
Purushotham Prajapati <Purushothamprajapati7473@gmail.com>
2026-02-04 01:02:15 +0530

    added favicon

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -8,7 +8,10 @@ const Loading = () => (
                 <div className="w-20 h-20 border-4 border-white/30 border-t-white rounded-full animate-spin mx-auto mb-4"></div>
                 <div className="absolute inset-0 w-16 h-16 border-4 border-transparent border-t-yellow-400 rounded-full animate-spin mx-auto mt-2 ml-2"></div>
             </div>
-            <h2 className="text-2xl font-bold mb-2">STI HUB</h2>
+            <div className="flex items-center justify-center">
+                <img src="favicon.png" alt=""className='h-20' />
+            </div>
+            <h2 className="text-2xl font-bold mb-2 text-white">STI HUB</h2>
             <p className="text-blue-200">Loading...</p>
         </div>
     </div>
